14492dac7f139351ac5c45db0e12d02a411b3cbc
test <text@gmail.com>
2021-04-14 22:15:35 +0800

    more practice

--- hello.py
+++ hello.py
@@ -1,18 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {}.</p>'.format(user_agent)
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
 
 
