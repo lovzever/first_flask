6c8aa1bbacd52c09a3c9ddbaea625f6f43ce6271
lovzever <lovzever1113@gmail.com>
2021-04-26 17:06:35 +0800

    practice over stage 1

---
+++ app/email.py
@@ -0,0 +1,20 @@
+from . import mail
+from flask_mail import Message
+from flask import render_template, current_app
+from threading import Thread
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    # mail.send(msg)
+    thr = Thread(target=send_async_email, args=[current_app, msg])
+    thr.start()
+    return thr
--- hello.py
+++ hello.py
@@ -9,22 +9,28 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail, Message
+from threading import Thread
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'lovzever'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+app.config['MAIL_SERVER'] = 'smtp.163.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = '13247150610@163.com'
+app.config['MAIL_PASSWORD'] = 'SJBDCFULELRIUGLJ'
+# app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+# app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
-app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
-app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky-liaojun <13247150610@163.com>'
+app.config['FLASKY_ADMIN'] = '1240837099@qq.com'
+# app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 
 bootstrap = Bootstrap(app)
@@ -64,12 +70,20 @@ def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    # mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -80,8 +94,10 @@ def index():
         if user is None:
             user = User(username=form.name.data)
             db.session.add(user)
-            db.session.commit()
+            # db.session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'NEW USER', 'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
