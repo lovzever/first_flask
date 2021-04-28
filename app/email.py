1edc839c7e7332eff8f098a2ceb7b652e18b5891
lovzever <lovzever1113@gmail.com>
2021-04-28 17:44:01 +0800

    user login finished

--- app/email.py
+++ app/email.py
@@ -1,12 +1,13 @@
-from . import mail
 from flask_mail import Message
+from . import mail
 from flask import render_template, current_app
 from threading import Thread
 
 
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
+# def send_async_email(app, msg):
+#     with app.app_context():
+#         print(app.name, '11111111111111111111111111111111111111111111')
+#         mail.send(msg)
 
 
 def send_email(to, subject, template, **kwargs):
@@ -14,7 +15,7 @@ def send_email(to, subject, template, **kwargs):
                   sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    # mail.send(msg)
-    thr = Thread(target=send_async_email, args=[current_app, msg])
-    thr.start()
-    return thr
+    mail.send(msg)
+    # thr = Thread(target=send_async_email, args=[current_app, msg])
+    # thr.start()
+    # return thr
--- app/main/views.py
+++ app/main/views.py
@@ -7,23 +7,28 @@ from ..models import User
 from ..email import send_email
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'NEW USER', 'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('main.index'))
-    return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False),
-                           current_time=datetime.utcnow())
+    return render_template('index.html')
+
+
+# @main.route('/', methods=['GET', 'POST'])
+# def index():
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.name.data).first()
+#         if user is None:
+#             user = User(username=form.name.data)
+#             db.session.add(user)
+#             db.session.commit()
+#             session['known'] = False
+#             if current_app.config['FLASKY_ADMIN']:
+#                 send_email(current_app.config['FLASKY_ADMIN'], 'NEW USER', 'mail/new_user', user=user)
+#         else:
+#             session['known'] = True
+#         session['name'] = form.name.data
+#         form.name.data = ''
+#         return redirect(url_for('main.index'))
+#     return render_template('index.html', form=form, name=session.get('name'),
+#                            known=session.get('known', False),
+#                            current_time=datetime.utcnow())
